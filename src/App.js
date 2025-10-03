6cd1eae46aee9f92ebe707a8551c450245616c70
Adam Cloyd Esparcia <adamcloydesparcia@gmail.com>
2025-10-04 01:40:02 +0800

    feature(cards): create layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicGrid from './Components/Grid';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Three Cards</h1>
+      <BasicGrid />
+      <button>DRAFT CARDS</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Grid.js
@@ -0,0 +1,37 @@
+import { Grid } from "@mui/material";
+
+export default function BasicGrid() {
+    return (
+        <Grid container sx={{ height: "500px", width: "1000px", border: "1px solid black" }}>
+            <CardsContainerGrid player={1} />
+            <CardsContainerGrid player={2}/>
+        </Grid>
+    );
+}
+
+export function CardsContainerGrid(props) {
+    return (
+        <Grid container size={6} sx={{ border: "1px solid red", padding: "10px" }}>
+            <CardsGrid />
+            <CardsGrid />
+            <CardsGrid />
+            <PlayerGrid player={props.player}/>
+        </Grid>
+    );
+}
+
+export function CardsGrid() {
+    return (
+        <Grid container size={4} sx={{ height: "280px", border: "1px solid black" }}>
+            
+        </Grid>
+    );
+}
+
+export function PlayerGrid(props) {
+    return (
+        <Grid size={12} sx={{ border: "1px solid blue" }}>
+            <h1 className="Player">Player {props.player}: </h1>
+        </Grid>
+    );
+}
